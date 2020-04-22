6aaffdb7f16f5693577c3dbc3a24ec05a23c6ea6
Robert Masiuk <robert@masiuk.pl>
2020-04-22 17:22:39 +0200

    select

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/ksiegarnia";
+        String user = "root";
+        String password = "";
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement mystmt = conn.createStatement();
+            String sql = "select * from klienci";
+            ResultSet rs = mystmt.executeQuery(sql);
+            while(rs.next()){
+                System.out.println(rs.getString("imie"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }}
